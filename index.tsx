fe643a2cd69bb679f3458ea825103275f9809824
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2021-02-23 14:29:05 -0800

    :tada: Initial commit.

---
+++ index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Button } from '@material-ui/core';
+
+ReactDOM.render(
+  <p>
+    <Button>PUSH</Button>
+    Hello world
+  </p>,
+  document.getElementById('app')
+);
